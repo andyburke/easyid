ad8bcd05109d37127bfebfdd0fcbfb90bb47e9b2
Andy Burke <andy.burke@mailbox.earth>
2020-09-14 18:49:12 -0700

    refactor: remove dependency on extend, update to use eslint

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
 'use strict';
 
-const extend = require( 'extend' );
-
 const DEFAULTS = {
-    length: 3,
-    groups: 3,
-    alphabet: '0123456789ABCDEFGHJKMNPQRTUVWXYZ',
-    separator: '-'
+	length: 3,
+	groups: 3,
+	alphabet: '0123456789ABCDEFGHJKMNPQRTUVWXYZ',
+	separator: '-'
 };
 
 module.exports = {
-    generate: ( _options ) => {
-        const options = extend( true, {}, DEFAULTS, _options );
+	generate: ( _options ) => {
+		const options = Object.assign( {}, DEFAULTS, _options );
 
-        const groups = Array.from( {
-            length: options.groups
-        }, () => {
-            return Array.from( {
-                length: options.length
-            }, () => {
-                return options.alphabet[ Math.floor( Math.random() * options.alphabet.length ) ];
-            } );
-        } );
-
-        return groups.map( group => group.join( '' ) ).join( options.separator );
-    }
+		return Array.from( {
+			length: options.groups
+		}, () => ( Array.from( {
+			length: options.length
+		}, () => ( options.alphabet[ Math.floor( Math.random() * options.alphabet.length ) ] ) ).join( '' ) ) ).join( options.separator );
+	}
 };
